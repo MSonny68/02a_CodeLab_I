495a6a3553dc4a0da4cc21b68ee517dacd5b0c3c
Maik Sonnenberg <maik.sonnenberg@hotmail.de>
2022-11-24 10:12:26 +0100

    2.Änderung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,9 +4,9 @@ public class App {
     
     public static void main(String[] args) {
 
-       Konto konto1 = new Konto(10000);
-       Konto konto2 = new Konto(10000);
-       Konto konto3 = new Konto(10000);
+       Konto konto1 = new Konto();  //Standartanfangskontostand = 5000
+       Konto konto2 = new Konto(100);  //für dieses Objekt Anfangskontostand
+       Konto konto3 = new Konto(2000);  //für dieses Objekt Anfangskontostand
 
        
             output("Anfangs-Kontostand für Konto1 beträgt: "+ String.valueOf(konto1.getAnfangsKontoStand()));
@@ -23,7 +23,21 @@ public class App {
         output("Kontostand für Konto1 beträgt: "+ String.valueOf(konto1.getKontoStand()));
         output("Kontostand für Konto2 beträgt: "+ String.valueOf(konto2.getKontoStand()));
         output("Kontostand für Konto3 beträgt: "+ String.valueOf(konto3.getKontoStand()));
+        
+        konto1.setKontoStand(konto1.getAnfangsKontoStand());
+        output("ursprünglicher Kontostand für Konto1 betrug: "+ String.valueOf(konto1.getKontoStand()));
+        konto2.setKontoStand(konto2.getAnfangsKontoStand());
+        output("ursprünglicher Kontostand für Konto2 betrug: "+ String.valueOf(konto2.getKontoStand()));
+        konto3.setKontoStand(konto3.getAnfangsKontoStand());
+        output("ursprünglicher Kontostand für Konto3 betrug: "+ String.valueOf(konto3.getKontoStand()));
 
+        konto1.setKontoStand(konto1.getKontoStand()*20);
+        konto2.setKontoStand(konto2.getKontoStand()*30);
+        konto3.setKontoStand(konto3.getKontoStand()*40);
+
+        output("Kontostand für Konto1 beträgt: "+ String.valueOf(konto1.getKontoStand()));
+        output("Kontostand für Konto2 beträgt: "+ String.valueOf(konto2.getKontoStand()));
+        output("Kontostand für Konto3 beträgt: "+ String.valueOf(konto3.getKontoStand()));
 
 
 
--- com/cc/java/Konto.java
+++ com/cc/java/Konto.java
@@ -2,10 +2,13 @@ package com.cc.java;
 public class Konto {
 
     private int kontoStand ;
-    private int anfangsKontoStand;
+    private int anfangsKontoStand = 5000;
 
     public Konto(int anfangsKontoStand) {
         this.anfangsKontoStand = anfangsKontoStand;
+    }
+    public Konto(){
+
     }
     
     public int getKontoStand() {
